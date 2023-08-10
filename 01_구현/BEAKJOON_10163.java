dbbe7372bd2b212362a15230e9075a1cefae257d
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-11 01:01:05 +0900

    BAEKJOON_10163_08/11/2023

---
+++ 01_구현/BEAKJOON_10163.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BEAKJOON_10163 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();// 종이 갯수
+
+		int[][] paper = new int[1001][1001];
+
+		for (int p = 1; p <= N; p++) {
+
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int xLen = sc.nextInt();
+			int yLen = sc.nextInt();
+
+			// 주어진 색종이 영역 채우기
+			for (int i = x; i < x+ xLen; i++) {
+				for (int j = y; j < y +yLen; j++) {
+					paper[i][j] = p;
+				}
+			}
+		}
+		for (int p = 1; p <= N; p++) {
+			int sum = 0;
+			for (int i = 0; i < 1001; i++) {
+				for (int j = 0; j < 1001; j++) {
+					if(paper[i][j] == p) {
+						sum++;
+					}
+				}
+			}
+			System.out.println(sum);
+		}
+
+	}
+}
