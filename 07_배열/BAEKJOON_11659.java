1a1f8fffdd718a9a8d9319585a3140cdc9100b02
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-15 16:17:27 +0900

    BAEKJOON_11659_08/15/2023

---
+++ 07_배열/BAEKJOON_11659.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BAEKJOON_11659 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); //N 개의 수
+		int M = sc.nextInt(); //M개의 구간합
+		
+		int[] sumArr = new int [N+1];
+		//구간합으로 배열 받기
+		for(int i = 1; i <=N; i++) {
+			sumArr[i] = sumArr[i-1] + sc.nextInt();
+		}
+		for(int i = 0; i < M; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			System.out.println(sumArr[end] - sumArr[start-1]);
+		}
+	}
+}
