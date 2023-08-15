11b73298a6d1f16e422e2e92af90b58ca56cfa58
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-15 17:15:59 +0900

    edit 03 folder

--- 03_완전탐색/src/BAEKJOON_2798_블랙잭/Solution.java
+++ 03_완전탐색/src/BAEKJOON_2798_블랙잭/Solution.java
@@ -1,6 +1,8 @@
+package BAEKJOON_2798_블랙잭;
+
 import java.util.Scanner;
 
-public class BAEKJOON_2798 {
+public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
