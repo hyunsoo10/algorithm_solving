03f39d76532d19d6e7366df9540471558a34c5f8
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-22 00:09:33 +0900

    BAEKJOON_1940_08/22/2023

---
+++ 02_수학/src/BAEKJOON_1940_주몽의명령/Main.java
@@ -0,0 +1,31 @@
+package BAEKJOON_1940_주몽의명령;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//재료의 개수 N
+		int N = sc.nextInt();
+		//갑옷을 만드는 데 필요한 수 M
+		int M = sc.nextInt();
+		
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int cnt = 0;//갑옷 만든 수
+		
+		
+		for(int i=0; i<N; i++) {
+			for(int j=i+1; j<N; j++) {
+				if (arr[i] + arr[j] == M) {
+					cnt++;//갑옷 수 증가
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
