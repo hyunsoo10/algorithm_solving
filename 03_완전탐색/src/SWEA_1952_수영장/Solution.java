f321e51c462945e8b0068c85128a65b9f71f817d
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-25 09:21:44 +0900

    SWEA_1952_08/25/2023

--- 03_완전탐색/src/SWEA_1952_수영장/Solution.java
+++ 03_완전탐색/src/SWEA_1952_수영장/Solution.java
@@ -3,51 +3,40 @@ package SWEA_1952_수영장;
 import java.util.Scanner;
 
 public class Solution {
-	public static int day, month, threeMonth, year;
-	public static int[] plan;
-	public static int answer;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for(int tc=1; tc<=T; tc++) {
-			day = sc.nextInt(); //1일 이용 요금
-			month = sc.nextInt(); //1개월 이용 요금
-			threeMonth = sc.nextInt();//3개월 이용 요금
-			year = sc.nextInt(); //1년 이용 요금
-			
-			//0번 인덱스 제외한 월 이용 요금 담을 배열
-			plan = new int[13];
 
-			for(int i=1; i<=12; i++) {
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int day = sc.nextInt(); // 1일 이용 요금
+			int month = sc.nextInt(); // 1개월 이용 요금
+			int threeMonth = sc.nextInt();// 3개월 이용 요금
+			int year = sc.nextInt(); // 1년 이용 요금
+
+			// 0번 인덱스 제외한 월 이용 날짜 담을 배열
+			int[] plan = new int[13];
+			// 이용할 월만 담은 리스트
+			for (int i = 1; i <= 12; i++) {
 				plan[i] = sc.nextInt();
 			}
-			
-			//1년 가격을 default로 정답 초기화
-			answer = year;
-			dfs(1, 0);
+
+			// 최소 비용 갱신해가면서 담을 dp 배열
+			int[] dp = new int[13];
+
+			// 1월 최소비용은 미리담아두기
+			dp[1] = Math.min(month, plan[1] * day);
+			dp[2] = Math.min(dp[1] + month, dp[1] + plan[2] * day);
+			// 3월 이후부터는 전월 최소 요금 + 해당 일 요금, 전월 최소요금 + 해당 월요금, 3개월 요금 셋 중에 비교해서 최솟값
+			for (int i = 3; i <= 12; i++) {
+				dp[i] = Math.min(dp[i - 3] + threeMonth, Math.min(dp[i - 1] + month, dp[i - 1] + plan[i] * day));
+			}
+			// dp[12]에 담긴 값과 1년 이용요금을 마지막으로 비교해서 더 작은 값을 정답으로 담는다.
+			int answer = year;
+			if (answer > dp[12])
+				answer = dp[12];
 			System.out.printf("#%d %d\n", tc, answer);
 
-
 		}
-		
-	}
-	public static void dfs(int mon, int costSum) {
-		if(answer <= costSum) return; //최소값을 넘어선 순간 더 볼 것도 없이 종료
-		
-		//탐색을 다 끝냈을 경우 두 값중 최솟값을 answer에 담는다.
-		if(mon > 12) {
-			answer = Math.min(answer, costSum);
-			return;
-		}
-		
-		//1일 이용권 이용할 경우
-		dfs(mon+1, costSum + plan[mon] * day);
-		//1개월 이용권
-		dfs(mon+1, costSum + month);
-		dfs(mon+3, costSum + threeMonth);
-		
 	}
 }
-
--- 03_완전탐색/src/SWEA_1952_수영장/Solution2.java
+++ 03_완전탐색/src/SWEA_1952_수영장/Solution2.java
@@ -1,67 +1,53 @@
 package SWEA_1952_수영장;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Solution2 {
+	public static int day, month, threeMonth, year;
+	public static int[] plan;
+	public static int answer;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int T = 1;
+		int T = sc.nextInt();
 		
 		for(int tc=1; tc<=T; tc++) {
-			int day = sc.nextInt(); //1일 이용 요금
-			int month = sc.nextInt(); //1개월 이용 요금
-			int threeMonth = sc.nextInt();//3개월 이용 요금
-			int year = sc.nextInt(); //1년 이용 요금
+			day = sc.nextInt(); //1일 이용 요금
+			month = sc.nextInt(); //1개월 이용 요금
+			threeMonth = sc.nextInt();//3개월 이용 요금
+			year = sc.nextInt(); //1년 이용 요금
 			
 			//0번 인덱스 제외한 월 이용 요금 담을 배열
-			int[] plan = new int[13];
-			//이용할 월만 담은 리스트
-			List<Integer> list = new ArrayList<>();
+			plan = new int[13];
+
 			for(int i=1; i<=12; i++) {
 				plan[i] = sc.nextInt();
-				if(plan[i]!=0)
-					list.add(i);
 			}
 			
-			int answer = 0;
+			//1년 가격을 default로 정답 초기화
+			answer = year;
+			dfs(1, 0);
+			System.out.printf("#%d %d\n", tc, answer);
 
-			Map<Integer, Integer> cost = new HashMap<>();
-			
-			//step1. day요금 담기
-			for(int i=0; i<list.size(); i++) {
-				cost.put(list.get(i), plan[list.get(i)] * day);
-			}
-			//step2. month요금이 더 적으면 month요금 담기
-			for(int i=0; i<list.size(); i++) {
-				if(cost.get(list.get(i)) > month)
-					cost.put(list.get(i), month);
-			}
-			//step1과 step2를 끝냈으면, 해당 월을 따로 계산 했을 때의 최저 금액을 일단 구했다.
-			
-			List<Integer> tmpList = new ArrayList<>();
-			//step3. 3개월 요금을 고려하자
-			for(int i=0; i<list.size(); i++) {
-				int tmp=0;
-				for(int j=0; j<list.size(); j++) {
-					//만약 연속된 숫자가 아니라면 그 월의 최저요금을 더해주자.
-					if(list.get(j)!=list.get(i)&&list.get(j)!=list.get(i)+1 && list.get(j)!=list.get(i)+2)
-						tmp += cost.get(list.get(j));
-				}
-				tmp += threeMonth;
-				tmpList.add(tmp);
-			}
-			
-			System.out.println(Arrays.toString(plan));
-			System.out.println(list.toString());
-			System.out.println(cost.toString());
-			System.out.println(tmpList.toString());
+
 		}
+		
+	}
+	public static void dfs(int mon, int costSum) {
+		if(answer <= costSum) return; //최소값을 넘어선 순간 더 볼 것도 없이 종료
+		
+		//탐색을 다 끝냈을 경우 두 값중 최솟값을 answer에 담는다.
+		if(mon > 12) {
+			answer = Math.min(answer, costSum);
+			return;
+		}
+		
+		//1일 이용권 이용할 경우
+		dfs(mon+1, costSum + plan[mon] * day);
+		//1개월 이용권
+		dfs(mon+1, costSum + month);
+		dfs(mon+3, costSum + threeMonth);
+		
 	}
 }
 
