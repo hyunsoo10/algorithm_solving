c7b5d440d743e6042d9b5e77336bafe274fe8988
hyunsoo10 <hyunsoo4885@gmail.com>
2023-08-25 17:36:12 +0900

    SWEA_5656_08/25/2023

---
+++ 01_구현/src/SWEA_5656_활주로_건설/Solution.java
@@ -0,0 +1,132 @@
+package SWEA_5656_활주로_건설;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T =  sc.nextInt();
+		for(int tc = 1; tc<=T; tc++) {
+			
+			int N = sc.nextInt(); //N*N맵
+			int X = sc.nextInt(); //경사로 길이
+			
+			int[][] map = new int[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					map[i][j] = sc.nextInt();
+				}
+			}//활주로 맵 정보 입력 끝
+			
+			int cnt = 0;
+			
+			//가로 탐색
+			for(int i=0; i<N; i++) {
+				boolean flag = true;//건설 가능 여부 체크 변수
+				//첫번째 시작 높이 초기화
+				int height = map[i][0];
+				int idx=0;//스타트시점 인덱스 
+				for(int j=1; j<N; j++) {
+					
+					
+					//높이 차이가 1보다 크면 건설 불가능
+					if(Math.abs(map[i][j]-height) > 1) {
+						flag = false;
+						break;
+					}
+					
+					//높이가 더 높아지는 경우
+					if(map[i][j] > height) {
+						//경사로 길이가 더 긴 경우 건설 불가능
+						if(j-idx < X) {
+							flag = false;
+							break;
+						}
+						//건설 성공시 높이랑 스타트시점 인덱스 최신화
+						height = map[i][j];
+						idx = j;
+					}
+					//높이가 더 낮아지는 경우
+					else if(map[i][j] < height) {
+						//낮아진 순간 그 높이와 같은 높이가 결사로의 길이 이상으로 유지되면 건설 가능
+						height = map[i][j];
+						int k = 1; //조회할 인덱스
+						int len=1;//길이는 1부터 시작
+						while(j+k < N && map[i][j+k]==height) {
+							len++;
+							k++;
+						}
+						//경사로의 길이보다 작으면 건설 불가능
+						if(len < X) {
+							flag = false;
+							break;
+						}
+						idx=j+X;
+						j=j+k-1;
+					}
+					//높이가 같으면 그냥 계속 가면 됌
+					else
+						continue;
+					
+				}//한 행 탐색 끝
+				if(flag)
+					cnt++;
+			}
+			
+			//세로 탐색
+			for(int j=0; j<N; j++) {
+				boolean flag = true;//건설 가능 여부 체크 변수
+				//첫번째 시작 높이 초기화
+				int height = map[0][j];
+				int idx=0;//스타트시점 인덱스 
+				for(int i=1; i<N; i++) {
+					
+					
+					//높이 차이가 1보다 크면 건설 불가능
+					if(Math.abs(map[i][j]-height) > 1) {
+						flag = false;
+						break;
+					}
+					
+					//높이가 더 높아지는 경우
+					if(map[i][j] > height) {
+						//경사로 길이가 더 긴 경우 건설 불가능
+						if(i-idx < X) {
+							flag = false;
+							break;
+						}
+						//건설 성공시 높이랑 스타트시점 인덱스 최신화
+						height = map[i][j];
+						idx = i;
+					}
+					//높이가 더 낮아지는 경우
+					else if(map[i][j] < height) {
+						//낮아진 순간 그 높이와 같은 높이가 결사로의 길이 이상으로 유지되면 건설 가능
+						height = map[i][j];
+						int k = 1; //조회할 인덱스
+						int len=1;//길이는 1부터 시작
+						while(i+k < N && map[i+k][j]==height) {
+							len++;
+							k++;
+						}
+						//경사로의 길이보다 작으면 건설 불가능
+						if(len < X) {
+							flag = false;
+							break;
+						}
+						idx=i+X;
+						i=i+k-1;
+					}
+					//높이가 같으면 그냥 계속 가면 됌
+					else
+						continue;
+					
+				}//한 행 탐색 끝
+				if(flag)
+					cnt++;
+			}
+			
+			System.out.printf("#%d %d\n",tc, cnt);
+		}
+	}
+}
--- 07_배열/src/SWEA_4014_활주로건설/Solution.java
+++ 07_배열/src/SWEA_4014_활주로건설/Solution.java
@@ -7,8 +7,8 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-//		int T = sc.nextInt();
-		int T = 1;
+		int T = sc.nextInt();
+//		int T = 1;
 
 		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt(); // N*N활주로
@@ -22,35 +22,115 @@ public class Solution {
 					map[i][j] = sc.nextInt();
 				}
 			}
+			// 활주로 건설 가능 여부 카운팅
+			int cnt1 = 0;
+			int cnt2 = 0;
 
-			// 가로 탐색
+			// 1. 가로 탐색
 			for (int i = 0; i < N; i++) {
-				//해당 구역 활주로 건설 가능 여부(true로 초기화)
+				// 해당 구역 활주로 건설 가능 여부(true로 초기화)
 				boolean flag = true;
+				
 				// 해당 가로의 가장 첫번째 구역 높이
-				int startHeight = map[i][0];
+				int height = map[i][0];
+				// 첫번째 인덱스
+				int idx = 0;
 				for (int j = 1; j < N; j++) {
-					// 첫번째 구역의 높이와 다른 구역이 나올 경우
-
-					// 1. 더 큰 높이인 경우
-					if (map[i][j] > startHeight) {
-						// 두 높이 차이가 1보다 크면 건설 불가능
-						if(map[i][j] - startHeight > 1) {
-							flag = false;
-							break;
+					// 두 높이의 차가 1보다 크면 활주로 건설 불가능
+					if (Math.abs(map[i][j] - height) > 1) {
+						flag = false;
+						break;
+					} else if (Math.abs(map[i][j] - height) == 1) {
+						// 큰 높이가 나올 경우
+						if (map[i][j] > height) {
+							if (x > j - idx) {
+								flag = false;
+								break;
+							}
+							//위치랑 높이 초기화
+							idx = j;
+							height = map[i][j];
 						}
-						//활주로 길이보다 더 짧은 경우
-						if(x > j) {
-							flag = false;
-							break;
+						// 작은 높이가 나올 경우
+						else {
+							int len = 1;
+							height = map[i][j];
+							idx = j+1;
+							while (idx < N && map[i][idx] == height) {
+								len++;
+								idx++;
+							}
+							
+							// 건설 가능 길이가 활주로 길이보다 짧은 경우 불가능
+							if (len < x) {
+								flag = false;
+								break;
+							}
+							//높이랑 위치 초기화
+							j = idx-1;
+							height = map[i][j];
 						}
 					}
-					// 2. 더 작은 높이인 경우
-					else if(map[i][j] < startHeight) {
-						
-					}
+					
 				}
+				if (flag)
+					cnt1++;
 			}
+			//세로 탐색
+			for (int j = 0; j < N; j++) {
+				// 해당 구역 활주로 건설 가능 여부(true로 초기화)
+				boolean flag = true;
+				
+				// 해당 가로의 가장 첫번째 구역 높이
+				int height = map[0][j];
+				// 첫번째 인덱스
+				int idx = 0;
+				for (int i = 1; i < N; i++) {
+					// 두 높이의 차가 1보다 크면 활주로 건설 불가능
+					if (Math.abs(map[i][j] - height) > 1) {
+						flag = false;
+						break;
+					} else if (Math.abs(map[i][j] - height) == 1) {
+						// 큰 높이가 나올 경우
+						if (map[i][j] > height) {
+							if (x > i - idx) {
+								flag = false;
+								break;
+							}
+							//위치랑 높이 초기화
+							idx = i;
+							height = map[i][j];
+							
+						}
+						// 작은 높이가 나올 경우
+						else {
+							int len = 1;
+							height = map[i][j];
+							idx = i+1;
+							while (idx < N && map[idx][j] == height) {
+								len++;
+								idx++;
+							}
+							
+							// 건설 가능 길이가 활주로 길이보다 짧은 경우 불가능
+							if (len < x) {
+//								System.out.println(i);
+								flag = false;
+								break;
+							}
+							//높이랑 위치 초기화
+							i = idx-1;
+							height = map[i][j];
+						}
+					}
+					
+				}
+				if (flag)
+					cnt2++;
+			}
+//			System.out.println(cnt1);
+//			System.out.println(cnt2);
+			System.out.println(cnt1+cnt2);
 
 		}
 	}
