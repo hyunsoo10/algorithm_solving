f136e88ffbaec04dbb3272d925cf86b9b01ce883
hyunsoo10 <hyunsoo4885@gmail.com>
2023-09-04 16:00:47 +0900

    BOJ_1629_09/04/2023

--- 11_분할정복/src/BOJ_1629_곱셈/Main.java
+++ 11_분할정복/src/BOJ_1629_곱셈/Main.java
@@ -11,23 +11,18 @@ public class Main {
 		B = sc.nextInt();
 		C = sc.nextInt();
 		
-		long ans = pow(1, B);
+		long ans = pow(A, B);
 		System.out.println(ans);
-		System.out.println(ans%C);
 	}
-	static long pow(long sum, int x) {
-		if(x == 1) return 1;
+	static long pow(long A, int x) {
+		if(x == 1) return A%C;
+		long tmp = pow(A, x/2);
 		//제곱을 짝수, 홀수에 따라 절반으로 나누기
+		//모듈러 연산으로 분할 정복 계산시에 미리 나머지 연산 해줘야 연산 범위를 안 벗어난다.
 		if(x%2==0) {
-			x = x/2;
-			long tmp = pow(A, x);
-			ans = tmp * tmp;
-			return ans;
+			return (tmp * tmp)%C;
 		}else {
-			int x1 = x/2;
-			int x2 = (x/2)+1;
-			ans = pow(A, x1) * pow(A, x2);
-			return ans;
+			return (tmp * tmp )%C *A%C;
 		}
 		
 	}
