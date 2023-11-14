ddb5aa07405c5654a0f5d0c2e965191dd377f27b
hyunsoo10 <hyunsoo4885@gmail.com>
2023-11-15 08:39:42 +0900

    BOJ_2075_11/15/2023

---
+++ 05_자료구조/src/BOJ_2075_N번째큰수/Main.java
@@ -0,0 +1,30 @@
+package BOJ_2075_N번째큰수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<N; j++) {
+				int x = Integer.parseInt(st.nextToken());
+				pq.add(x);
+			}
+		}
+		
+		for(int i=0; i<N-1; i++) {
+			pq.poll();
+		}
+		System.out.println(pq.poll());
+	}
+}
